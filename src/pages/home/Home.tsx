5558c3d1a7c78d488f0518fd14cf179be00b1915
Eduardo Feitosa <edudfeitosa@outlook.com>
2024-08-21 01:46:13 -0300

    configuração da estrutura

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => (
+    <>Hello World</>
+);
+
+export default Home;
---
+++ src/ui/components/surfaces/header/Header.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+const Header = () => (
+    <header className={styles.header}>
+        Header do projetin
+    </header>
+);
+
+export default Header;
